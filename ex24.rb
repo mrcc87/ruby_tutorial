7f139d60dd9880b27106ff05d4e0099bb79f9235
mrcc87 <mrcc87@gmail.com>
2015-01-03 12:21:04 +0100

    comments added to ex24.rb

--- ex24.rb
+++ ex24.rb
@@ -1,6 +1,10 @@
+# This line will be printed on screen.
 puts "Let's practice everything."
-puts "You\' d need to know \'bout escapes with \\ that do \\n newlines and \\t tabs."
 
+# The \ will escape special characters ' \ \n \t.
+puts "You\'d need to know \'bout escapes with \\ that do \\n newlines and \\t tabs."
+
+# The block in <<END ... END will be stored as writen in the poem variable.
 poem = <<END
 \tThe lovely world
 with logic so firmly planted
@@ -10,12 +14,15 @@ an requires an explanation
 \n\t\twhere there is none.
 END
 
+# This will print the poem variable as it was stored before with tabs and newlines.
 puts "---------------"
 puts poem
 puts "---------------"
 
+# This will calculate and print the number 5.
 five = 10 - 2 + 3 - 6
 puts "This should be #{five}."
+
 
 def secret_formula(started)
   jelly_beans = started * 500
